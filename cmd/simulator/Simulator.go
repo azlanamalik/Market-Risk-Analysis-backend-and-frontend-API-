f9011040405aaa8a2404957d33c7b83685f66a5c
Azlan <ec23400@qmul.ac.uk>
2026-09-15 16:20:17 +0100

    added a skeleton outside

---
+++ cmd/simulator/Simulator.go
@@ -0,0 +1,11 @@
+package simulator
+
+import (
+	"fmt"
+)
+
+
+
+func Run() {
+	fmt.Println("initialising the simulator")
+}
---
+++ main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/azlanamalik/Market-Risk-Analysis-backend-and-frontend-API-/cmd/simulator"
+)
+
+func main() {
+	simulator.Run()
+}
