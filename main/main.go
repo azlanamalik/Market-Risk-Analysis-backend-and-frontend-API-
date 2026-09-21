5727c5589cdc15bcb9b2d5e8b31e461e70858282
Azlan <ec23400@qmul.ac.uk>
2026-09-21 18:26:38 +0100

    running program now there is an issue with storing the pricetick

--- main/main.go
+++ main/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/azlanamalik/Market-Risk-Analysis-backend-and-frontend-API-/cmd/simulator"
+	"context"
 )
 
 func main() {
-	simulator.Run()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	listOfDat := [] string {"AAPL","MSFT","GOOGL","AMZN","TSLA"}
+	simulator.Run(ctx,listOfDat)
+
 }
